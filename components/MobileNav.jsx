968533fd1d9128f60b7ce67823ba2df7206a4d48
HICINTUKA <hicintuka@Hicintukas-MacBook-Pro.local>
2025-01-03 15:01:26 -0500

    adding descrition and buttons to homepage

---
+++ components/MobileNav.jsx
@@ -0,0 +1,80 @@
+"use client";
+import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Services",
+    path: "/services",
+  },
+  {
+    name: "Resume",
+    path: "/resume",
+  },
+  {
+    name: "Work",
+    path: "/work",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex justify-center items-center"
+      >
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <SheetTitle className="mt-32 mb-40 text-center text-2xl">
+          <Link onClick={handleLinkClick} href="/">
+            <h1 className="text-white text-4xl font-semibold">
+              Philip<span className="text-accent italic text-3xl">dev</span>
+            </h1>
+          </Link>
+        </SheetTitle>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                key={index}
+                href={link.path}
+                onClick={handleLinkClick}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
