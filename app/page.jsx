4a6539f9bf695e6618b303e6ea02cc6d372b7610
HICINTUKA <hicintuka@Hicintukas-MacBook-Pro.local>
2025-01-03 19:46:59 -0500

    adding animation on profile image

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,14 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
-
+import Stats from "@/components/Stats";
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text  */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl"> Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm
@@ -41,11 +41,12 @@ const Home = () => {
             </div>
           </div>
           {/* photo  */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,35 +12,35 @@ export default {
       padding: "15px",
     },
     screens: {
-      sm:'640px',
-      md:'768px',
-      lg:'960px',
-      xl:'1200px'
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     fontFamily: {
-      primary: "var(--font-jetbrainsMono)"
+      primary: "var(--font-jetbrainsMono)",
     },
-  	extend: {
-  		colors: {
-        primary: '#0a0a0a',
+    extend: {
+      colors: {
+        primary: "#101010", //background
+        // primary: '#0a0a0a',
         accent: {
-          DEFAULT: '#00ff99',
-          hover: '#00e187'
-        }
-  		},
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+      },
       borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	},
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
     backgroundColor: (theme) => ({
-      ...theme('colors'),
+      ...theme("colors"),
     }),
     borderColor: (theme) => ({
-      ...theme('colors'),
+      ...theme("colors"),
     }),
-
   },
   plugins: [require("tailwindcss-animate")],
 };
